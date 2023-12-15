c7b17926b0894786a235ec4411b0b431ccf16d47
legendryflyer <tavishanade12@gmail.com>
2023-12-15 23:46:25 +0530

    13-12-23.js

---
+++ 13-12-23.js
@@ -0,0 +1,23 @@
+// strings ...
+
+
+let names="tavish"
+console.log(names)
+
+//properties and method 
+//0   1    2     3 
+//p   u    n     e
+
+
+let city="pune"
+let q=city.length
+console.log(q)
+
+// length -1 is always a last index 
+
+console.log(city.toUpperCase())
+
+city2="Nagpur"
+console.log(city2.toLowerCase())
+
+
